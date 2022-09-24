87cfa852ca70e109cc1ad4182c7f65c568b2c30e
송혜경 <sosschs9@naver.com>
2022-09-25 03:34:55 +0900

    flask

---
+++ app.py
@@ -0,0 +1,30 @@
+import requests
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("main.html")
+
+@app.route('/reserve')
+def reserve():
+
+    return render_template('reserve.html')
+
+@app.route('/bus_watch', methods=['POST'])
+def bus_watch():
+    error = None
+    if request.method == 'POST':
+        busname = request.form['busname']
+        return render_template('bus_watch.html', name=busname)
+
+@app.route('/bus_station', methods=['POST'])
+def bus_station():
+    data = requests.get("http://localhost:8080/station/search/" + request.form['bus_station'])
+    busdata = data.json()
+    return render_template('bus_station.html', data=busdata)
+
+if __name__ == '__main__':
+    app.run()
